e11213ddb6708a73ada79a4d2ffd4125a308ff00
Zombiuu <ramses.diez@live.u-tad.com>
2018-01-21 21:23:42 +0100

    Pruebas, segui tutorial para verlo de otras maneras, no hate :D

--- app/src/main/java/com/example/ramsesdiezgalvan/act5pmdm_parte1/MainActivity.java
+++ app/src/main/java/com/example/ramsesdiezgalvan/act5pmdm_parte1/MainActivity.java
@@ -1,31 +1,115 @@
 package com.example.ramsesdiezgalvan.act5pmdm_parte1;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-import com.example.ramsesdiezgalvan.act5pmdm_parte1.asynTasks.HttpJson;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity{
-    MainActivityEvents events;
+    JSONObject jsonobject;
+    JSONArray jsonarray;
+    ProgressDialog mProgressDialog;
+    ArrayList<String> worldlist;
+    ArrayList<WorldPopulation> world;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        events = new MainActivityEvents();
-
-        HttpJson httpJson = new HttpJson();
-
-        httpJson.setListener(events);
-
-        String url = "http://api.population.io:80/1.0/countries";
-
-        httpJson.execute(url);
-
-
-
+        // Download JSON file AsyncTask
+        new DownloadJSON().execute();
 
     }
 
+    // Download JSON file AsyncTask
+    private class DownloadJSON extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            // Locate the WorldPopulation Class
+            world = new ArrayList<WorldPopulation>();
+            // Create an array to populate the spinner
+            worldlist = new ArrayList<String>();
+            // JSON file URL address
+            jsonobject = JSONfunctions
+                    .getJSONfromURL("http://www.androidbegin.com/tutorial/jsonparsetutorial.txt");
+
+            try {
+                // Locate the NodeList name
+                jsonarray = jsonobject.getJSONArray("worldpopulation");
+                for (int i = 0; i < jsonarray.length(); i++) {
+                    jsonobject = jsonarray.getJSONObject(i);
+
+                    WorldPopulation worldpop = new WorldPopulation();
+
+                    worldpop.setRank(jsonobject.optString("rank"));
+                    worldpop.setCountry(jsonobject.optString("country"));
+                    worldpop.setPopulation(jsonobject.optString("population"));
+                    worldpop.setFlag(jsonobject.optString("flag"));
+                    world.add(worldpop);
+
+                    // Populate spinner with country names
+                    worldlist.add(jsonobject.optString("country"));
+
+                }
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void args) {
+            // Locate the spinner in activity_main.xml
+            Spinner mySpinner = (Spinner) findViewById(R.id.my_spinner);
+
+            // Spinner adapter
+            mySpinner
+                    .setAdapter(new ArrayAdapter<String>(MainActivity.this,
+                            android.R.layout.simple_spinner_dropdown_item,
+                            worldlist));
+
+            // Spinner on item click listener
+            mySpinner
+                    .setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+                        @Override
+                        public void onItemSelected(AdapterView<?> arg0,
+                                                   View arg1, int position, long arg3) {
+                            // TODO Auto-generated method stub
+                            // Locate the textviews in activity_main.xml
+                            TextView txtrank = (TextView) findViewById(R.id.rank);
+                            TextView txtcountry = (TextView) findViewById(R.id.country);
+                            TextView txtpopulation = (TextView) findViewById(R.id.population);
+
+                            // Set the text followed by the position
+                            txtrank.setText("Rank : "
+                                    + world.get(position).getRank());
+                            txtcountry.setText("Country : "
+                                    + world.get(position).getCountry());
+                            txtpopulation.setText("Population : "
+                                    + world.get(position).getPopulation());
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> arg0) {
+                            // TODO Auto-generated method stub
+                        }
+                    });
+        }
+    }
 
 }
--- app/src/main/java/com/example/ramsesdiezgalvan/act5pmdm_parte1/MainActivityEvents.java
+++ app/src/main/java/com/example/ramsesdiezgalvan/act5pmdm_parte1/MainActivityEvents.java
@@ -1,18 +1,29 @@
 package com.example.ramsesdiezgalvan.act5pmdm_parte1;
 
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.ramsesdiezgalvan.act5pmdm_parte1.asynTasks.HttpJsonListener;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  * Created by ramsesdiezgalvan on 19/1/18.
  */
 
 public class MainActivityEvents implements HttpJsonListener {
+    MainActivity mainActivity;
 
+
+
+    public MainActivityEvents(MainActivity mainActivity){
+        this.mainActivity = mainActivity;
+    }
 
     @Override
     public void jsonDownload(String s) {
@@ -20,7 +31,9 @@ public class MainActivityEvents implements HttpJsonListener {
         try {
             JSONObject jsonObject = new JSONObject(s);
 
-            System.out.println( jsonObject.getJSONArray("countries"));
+//
+
+          //  System.out.println( jsonObject.getJSONArray("countries"));
 
 
         } catch (Exception e) {
